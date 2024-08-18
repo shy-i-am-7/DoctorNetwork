39c152b8903ea335bee2ed52741ef84ff4467ca7
Naveen <chithannaveen@gmail.com>
2024-08-18 13:22:59 -0400

    Added basic Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar"
 import Header from './components/layout/Header';
 import DoctorOfMonth from "./pages/DoctorOfMonth"
 import PreviousDoc from "./pages/PreviousDoc"
 import Newsletter from "./pages/Newsletter"
 import contact from "./pages/Contact"
+import Footer from "./components/layout/Footer";
+
 function App() {
   return (
     <Router>
@@ -17,6 +19,7 @@ function App() {
         <Route path='/Newsletter' Component={Newsletter} />
         <Route path='/contact' Component={contact} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="header-logo">
+        <img src="https://img.icons8.com/ios-filled/50/ffffff/stethoscope.png" alt="Logo" />
+      </div>
+      <div className="header-title">
+        Doctor Network Footer
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
